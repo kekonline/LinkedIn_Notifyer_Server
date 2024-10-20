ecc613f39091c4a863f3f1e6cf6fc918ceeb575f
Keko <kekonline@gmail.com>
2024-10-20 16:43:04 +0200

    added screperjob function in main and other minor tweaks

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { scrapeJobListing, scrapeJobDescription } from "../src/scrapers/scrapeJob";
+import { MailService } from "../src/mail/mail.service";
+import { deleteUsersWithoutEmailAndOldCreatedDate } from "../src/maintenance/cleanDB";
+
+const interval = 5 * 60 * 1000; // 5 minutes in milliseconds
+let isRunning = false;
+
 
 async function bootstrap() {
   dotenv.config();
@@ -9,5 +16,34 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+
+  // setInterval(serverJobs, interval);
+  // serverJobs();
 }
+
+async function serverJobs() {
+  const mailService = new MailService();
+  if (isRunning) {
+    console.log("Scraping tasks are already running. Skipping this interval.");
+    return;
+  }
+  isRunning = true;
+
+  try {
+    console.log("Starting scraping tasks...");
+    await scrapeJobListing();
+    await scrapeJobDescription();
+    await mailService.sendJobsMail();
+    await deleteUsersWithoutEmailAndOldCreatedDate();
+    console.log("Scraping tasks completed.");
+  } catch (error) {
+    console.error("An error occurred during scraping tasks:", error);
+  } finally {
+    isRunning = false;
+  }
+
+}
+
+
 bootstrap();
---
+++ src/maintenance/cleanDB.ts
@@ -0,0 +1,42 @@
+
+
+import { UserSchema } from '../user/schemas/user.schema';
+import { SearchTermSchema } from '../search-term/schemas/search-term.schema';
+import * as mongoose from 'mongoose';
+const User = mongoose.model('User', UserSchema);
+const SearchTerm = mongoose.model('SearchTerm', SearchTermSchema);
+
+
+export const deleteUsersWithoutEmailAndOldCreatedDate = async () => {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    try {
+        const usersWithoutEmail = await User.find({
+            email: { $exists: false },
+            createdAt: { $lt: oneWeekAgo }
+        });
+
+        const usersToDelete = [];
+
+        for (const user of usersWithoutEmail) {
+            const hasSearchTerms = await SearchTerm.exists({
+                users: user._id
+            });
+
+            if (!hasSearchTerms) {
+                usersToDelete.push(user._id);
+            }
+        }
+
+        if (usersToDelete.length > 0) {
+            await User.deleteMany({ _id: { $in: usersToDelete } });
+            console.log(`${usersToDelete.length} users deleted without email and no search terms`);
+        } else {
+            console.log("No users found for deletion");
+        }
+
+    } catch (error) {
+        console.error("Error deleting users:", error);
+    }
+};
