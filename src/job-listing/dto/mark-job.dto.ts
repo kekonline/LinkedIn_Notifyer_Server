d02a396ba5dfac43aa0f939bd6fedf71d86c3298
Keko <kekonline@gmail.com>
2024-10-22 19:00:54 +0200

    first next js commit

--- src/job-listing/dto/mark-job.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsIn } from 'class-validator';
-
-export class MarkJobDto {
-    @IsString()
-    @IsIn(['seen', 'starred']) // Only allow 'seen' or 'starred' values
-    markAs: 'seen' | 'starred';
-}
