b479ba1f7e74611bfc80d7d63649d20a574f1fb2
Keko <kekonline@gmail.com>
2024-11-09 20:01:16 +0100

    fixed issue in next js db conexion and scrape jobs was called many times

--- app/utils/dbConfig.ts
+++ app/utils/dbConfig.ts
@@ -1,19 +1,38 @@
-// app/lib/mongoConnect.ts
 import mongoose from "mongoose";
 
-export const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/LinkedIn_Notifyer";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/LinkedIn_Notifyer";
 
-export const connectToMongo = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return; // Prevent multiple connections
+class MongoDBConnection {
+  private static instance: MongoDBConnection;
+  private isConnected = false;
+
+  private constructor() {
   }
 
-  try {
-    const db = await mongoose.connect(MONGO_URI);
-    const dbName = db.connections[0].name;
-    console.log(`Connected To Mongo! Database Name: ${dbName}`);
-  } catch (err) {
-    console.error("Error connecting to MongoDB: ", err);
+  public static getInstance(): MongoDBConnection {
+    if (!MongoDBConnection.instance) {
+      MongoDBConnection.instance = new MongoDBConnection();
+    }
+    return MongoDBConnection.instance;
   }
-};
+
+  public async connect() {
+    if (this.isConnected || mongoose.connection.readyState >= 1) {
+      console.log("Using existing MongoDB connection.");
+      return;
+    }
+
+    try {
+      const db = await mongoose.connect(MONGO_URI);
+      this.isConnected = true;
+      const dbName = db.connections[0].name;
+      console.log(`Connected To Mongo! Database Name: ${dbName}`);
+    } catch (err) {
+      console.error("Error connecting to MongoDB: ", err);
+      throw err;
+    }
+  }
+}
+
+
+export const connectToMongo = MongoDBConnection.getInstance();
